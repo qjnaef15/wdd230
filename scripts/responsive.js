7fe46afc0a0f9385f0cffef14e1045d7630fa34b
qjnaef15 <nae15002@byui.edu>
2023-09-22 17:46:18 -0700

    responsive ham menu added

---
+++ scripts/responsive.js
@@ -0,0 +1,10 @@
+// Get references to the button and the navigation menu
+const hamburgerButton = document.getElementById("hamburger-button");
+const navigationMenu = document.querySelector("nav ul");
+
+// Add a click event listener to the hamburger button
+hamburgerButton.addEventListener("click", function () {
+    // Toggle the visibility of the navigation menu
+    navigationMenu.classList.toggle("show-menu");
+    hamburgerButton.classList.toggle('active');
+});
