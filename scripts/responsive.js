cfb902f9d66974c18716cfa46f3cab987aced2f5
qjnaef15 <nae15002@byui.edu>
2023-10-16 09:56:37 -0700

    accessibility changes

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,5 +1,5 @@
 // Get references to the button and the navigation menu
-const hamburgerButton = document.getElementById("hamburger-button");
+const hamburgerButton = document.getElementById("hamburger-menu");
 const navigationMenu = document.querySelector("nav ul");
 
 // Add a click event listener to the hamburger button
