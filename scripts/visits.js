db0f86d7f683201963662cf532ad5deab7804426
qjnaef15 <nae15002@byui.edu>
2023-10-25 11:59:42 -0700

    visits implemented

---
+++ scripts/visits.js
@@ -0,0 +1,13 @@
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("num-Visits")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🎉 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("num-Visits", numVisits);
