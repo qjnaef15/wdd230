b4f7cf69c24a4094b33768c83f0f04e0154d9ea6
qjnaef15 <nae15002@byui.edu>
2023-11-11 11:10:42 -0800

    js fix

--- chamber/scripts/timestamp.js
+++ chamber/scripts/timestamp.js
@@ -1,5 +1,13 @@
-// JavaScript to populate the timestamp field with the current date and time
-const timestampField = document.getElementById("timestamp");
-const now = new Date();
-const timestamp = now.toISOString();
-timestampField.value = timestamp;
+function setTimestamp() {
+    // Get the input field
+    var timestampInput = document.getElementById("timestamp");
+
+    // Create a new Date object to get the current timestamp
+    var currentTimestamp = new Date();
+
+    // Assign the current timestamp to the input field's value
+    timestampInput.value = currentTimestamp.getTime();
+}
+
+// Add event listener to the button with id "submit"
+document.getElementById("submit").addEventListener("click", setTimestamp);
