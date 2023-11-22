ab26bc713f2a0eb05b3d1381ea6ed4e0dcdbcd35
qjnaef15 <nae15002@byui.edu>
2023-11-22 14:05:47 -0800

    styles fixes with darkmode

--- chamber/scripts/responsive.js
+++ chamber/scripts/responsive.js
@@ -7,3 +7,21 @@ hamburger.addEventListener('click', () => {
     nav.classList.toggle('responsive');
 
 });
+
+// Add active class to navigation links
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the current page URL
+    var currentPage = window.location.pathname.split("/").pop();
+
+    // Remove arrows from all links
+    var arrowSpans = document.querySelectorAll(".arrow");
+    arrowSpans.forEach(function(arrow) {
+      arrow.style.display = "none";
+    });
+
+    // Show arrow on the link corresponding to the current page
+    var currentArrow = document.getElementById(currentPage + "-arrow");
+    if (currentArrow) {
+      currentArrow.style.display = "inline-block";
+    }
+  });
