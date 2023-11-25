11e4a43f1a0d34ae9068693b67bfbdcaea2b97e4
qjnaef15 <nae15002@byui.edu>
2023-11-25 10:24:59 -0800

    changes spotlight to add fetch

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,32 +1,35 @@
-// Assuming you have loaded the members.json data into a variable named 'membersData'
+fetch('data/members.json')
+        .then(response => response.json())
+        .then(membersData => {
+            // Filter members with gold or silver membership
+            const goldSilverMembers = membersData.members.filter(member =>
+                member.membership.level === "Gold" || member.membership.level === "Silver"
+            );
 
-    // Filter members with gold or silver membership
-    const goldSilverMembers = membersData.members.filter(member =>
-        member.membership.level === "Gold" || member.membership.level === "Silver"
-    );
+            // Shuffle the array to get random members every load
+            const shuffledMembers = goldSilverMembers.sort(() => Math.random() - 0.5);
 
-    // Shuffle the array to get random members every load
-    const shuffledMembers = goldSilverMembers.sort(() => Math.random() - 0.5);
+            // Take the first three members
+            const selectedMembers = shuffledMembers.slice(0, 3);
 
-    // Take the first three members
-    const selectedMembers = shuffledMembers.slice(0, 3);
+            // Insert members into the HTML
+            const spotlightsSection = document.getElementById("spotlights");
 
-    // Insert members into the HTML
-    const spotlightsSection = document.getElementById("spotlights");
+            selectedMembers.forEach(member => {
+                const spotlightHTML = `
+                    <div>
+                        <img src="${member.image}" alt="${member.name}">
+                        <h4>${member.name}</h4>
+                        <p>${member.otherInfo}</p>
+                        <p>${member.membership.level} Member</p>
+                        <a href="${member.website}">${member.name}</a>
+                    </div>
+                `;
 
-    selectedMembers.forEach(member => {
-        const spotlightHTML = `
-            <div>
-                <img src="${member.image}" alt="${member.name}">
-                <h4>${member.name}</h4>
-                <p>${member.otherInfo}</p>
-                <p>${member.membership.level} Member</p>
-                <a href="${member.website}">${member.name}</a>
-            </div>
-        `;
+                const spotlightElement = document.createElement("div");
+                spotlightElement.innerHTML = spotlightHTML;
 
-        const spotlightElement = document.createElement("div");
-        spotlightElement.innerHTML = spotlightHTML;
-
-        spotlightsSection.appendChild(spotlightElement);
-    });
+                spotlightsSection.appendChild(spotlightElement);
+            });
+        })
+        .catch(error => console.error('Error fetching members.json:', error));
