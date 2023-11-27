35f4df29da2893013263951deb7cc2d4094cf2bc
qjnaef15 <nae15002@byui.edu>
2023-11-27 15:49:52 -0800

    fixes on dynamic spotlights

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -7,7 +7,7 @@ fetch('data/members.json')
         );
 
         // Shuffle the array to get random members every load
-        const shuffledMembers = goldSilverMembers.sort(() => Math.random() - 0.5);
+        const shuffledMembers = getRandomMembers(goldSilverMembers);
 
         // Take the first three members
         const selectedMembers = shuffledMembers.slice(0, 3);
@@ -17,6 +17,12 @@ fetch('data/members.json')
 
         // Insert members into the HTML
         const spotlightsSection = document.getElementById("spotlights");
+        spotlightsSection.innerHTML = ''; // Clear previous content
+
+        // Add a title for Spotlights
+        const titleElement = document.createElement("h2");
+        titleElement.innerText = "Spotlights";
+        spotlightsSection.appendChild(titleElement);
 
         selectedMembers.forEach(member => {
             // Define background, text, and link colors based on light or dark mode
@@ -26,11 +32,13 @@ fetch('data/members.json')
 
             const spotlightHTML = `
                 <div class="spotlight-item" style="background-color: ${backgroundColor}; color: ${textColor};">
-                    <img src="${member.image}" alt="${member.name}">
                     <h4>${member.name}</h4>
+                    <div class="image-container">
+                        <img src="${member.image}" alt="${member.name}" style="width: 100px; height: 100px;">
+                    </div>
                     <p>${member.address}</p>
                     <p>${member.membership.level} Member</p>
-                    <a href="${member.website}" style="color: ${linkColor};">${member.name}</a>
+                    <a href="${member.website}" style="color: ${linkColor};">Visit Website</a>
                 </div>
             `;
 
@@ -44,3 +52,13 @@ fetch('data/members.json')
         });
     })
     .catch(error => console.error('Error fetching members.json:', error));
+
+// Function to shuffle array elements randomly
+function getRandomMembers(array) {
+    const shuffled = array.slice(0);
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}
