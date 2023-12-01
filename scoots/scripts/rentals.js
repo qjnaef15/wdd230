44903484d15cff94643edf0e369cff0d9401426e
qjnaef15 <nae15002@byui.edu>
2023-12-01 15:05:39 -0800

    table fixes

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -1,53 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tableBody = document.getElementById("rentalTableBody");
+    const reservationTableBody = document.getElementById("reservationTableBody");
+    const walkInTableBody = document.getElementById("walkInTableBody");
 
     fetch('data/rentals.json')
         .then(response => response.json())
         .then(data => {
             data.rentalPrices.forEach(rental => {
-                const row = document.createElement("tr");
-                if (isMobileView()) {
-                    // If in mobile view, create separate tables for reservations and walk-ins
-                    createTableForMobile(row, rental.reservation, 'Reservation');
-                    createTableForMobile(row, rental.walkIn, 'Walk-In');
-                } else {
-                    // If not in mobile view, display both reservation and walk-in information in the same row
-                    row.innerHTML = `
-                        <td>${rental.rentalType}</td>
-                        <td>${rental.maxPersons}</td>
-                        <td>${rental.reservation.halfDay}</td>
-                        <td>${rental.reservation.fullDay}</td>
-                        <td>${rental.walkIn.halfDay}</td>
-                        <td>${rental.walkIn.fullDay}</td>
-                    `;
-                }
-                tableBody.appendChild(row);
+                // Create a row for reservations
+                const reservationRow = document.createElement("tr");
+                reservationRow.innerHTML = `
+                    <td>${rental.rentalType}</td>
+                    <td>${rental.maxPersons}</td>
+                    <td>${rental.reservation.halfDay}</td>
+                    <td>${rental.reservation.fullDay}</td>
+                `;
+                reservationTableBody.appendChild(reservationRow);
+
+                // Create a row for walk-ins
+                const walkInRow = document.createElement("tr");
+                walkInRow.innerHTML = `
+                    <td>${rental.rentalType}</td>
+                    <td>${rental.maxPersons}</td>
+                    <td>${rental.walkIn.halfDay}</td>
+                    <td>${rental.walkIn.fullDay}</td>
+                `;
+                walkInTableBody.appendChild(walkInRow);
             });
         })
         .catch(error => console.error('Error fetching JSON:', error));
 
     // Function to check if the screen size is in mobile view
     function isMobileView() {
-        return window.innerWidth <= 430; 
+        return window.innerWidth <= 600; // You can adjust this value based on your design
     }
 
-    // Function to create a separate table for mobile view
-    function createTableForMobile(row, data, label) {
-        const mobileTable = document.createElement("table");
-        mobileTable.innerHTML = `
-            <tr>
-                <td>${label} Type</td>
-                <td>${label} Half Day</td>
-                <td>${label} Full Day</td>
-            </tr>
-            <tr>
-                <td>${data.type}</td>
-                <td>${data.halfDay}</td>
-                <td>${data.fullDay}</td>
-            </tr>
-        `;
-        const cell = document.createElement("td");
-        cell.appendChild(mobileTable);
-        row.appendChild(cell);
+    // Toggle table visibility based on screen size
+    function toggleTableVisibility() {
+        const reservationTable = document.getElementById("reservationTable");
+        const walkInTable = document.getElementById("walkInTable");
+
+        if (isMobileView()) {
+            reservationTable.style.display = "table";
+            walkInTable.style.display = "none";
+        } else {
+            reservationTable.style.display = "table";
+            walkInTable.style.display = "table";
+        }
     }
+
+    // Initial visibility check
+    toggleTableVisibility();
+
+    // Listen for window resize events to update table visibility
+    window.addEventListener("resize", toggleTableVisibility);
 });
