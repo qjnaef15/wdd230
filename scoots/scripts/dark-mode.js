c3d9aacfae08c0314cd7204d2ff98e4fc9390eac
qjnaef15 <nae15002@byui.edu>
2023-12-07 11:04:30 -0800

    added dark mode preference for user

--- scoots/scripts/dark-mode.js
+++ scoots/scripts/dark-mode.js
@@ -1,14 +1,25 @@
 function darkmode() {
     const body = document.body;
-    body.classList.toggle("dark-mode");
-
-    // Optionally, you can also update other elements on the page based on the dark mode state
     const modeIcon = document.getElementById("mode-icon");
-    if (body.classList.contains("dark-mode")) {
-        // Dark mode is active
+    const isDarkMode = body.classList.toggle("dark-mode");
+
+    // Update the mode icon based on the dark mode state
+    if (isDarkMode) {
         modeIcon.src = "images/moon.svg"; // Replace with the moon icon or any other dark mode icon
     } else {
-        // Light mode is active
         modeIcon.src = "images/sun.svg"; // Replace with the sun icon or any other light mode icon
     }
-};
+
+    // Save the user's preference to localStorage
+    localStorage.setItem("darkMode", isDarkMode.toString());
+}
+
+// Check for user preference on page load
+document.addEventListener("DOMContentLoaded", () => {
+    const savedDarkMode = localStorage.getItem("darkMode");
+
+    if (savedDarkMode && savedDarkMode === "true") {
+        document.body.classList.add("dark-mode");
+        document.getElementById("mode-icon").src = "images/moon.svg"; // Replace with the moon icon or any other dark mode icon
+    }
+});
